669a47b37ffddc030f7311aabf16c40f3e9d77e5
Stephan Robotta <stephan.robotta@bfh.ch>
2025-10-20 13:10:26 +0200

    Add script to list complete glossary articles.

---
+++ complete_glossary_article.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+
+use KnowledgeBase\Kb;
+
+/* @var KnowledgeBase\Article $article */
+foreach (Kb::getArticles() as $article) {
+    if (!$article->isGlossarEntry()) {
+        continue;
+    }
+    $headlines = $article->getH2List();
+    if (count($headlines) === 5) {
+        echo 'Article link: ' . $article->permalink() . PHP_EOL;
+        foreach ($headlines as $headline) {
+            echo ' - ' . $headline . PHP_EOL;
+        }
+    }
+}
